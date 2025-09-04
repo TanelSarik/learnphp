18c5eaa0d82e30aface5e4da35bdee2ea56f986d
Tanel Sarik <tanel.sarik@tptlive.ee>
2025-09-04 10:13:57 +0300

    add on variables

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Hello world\n" ;
+echo "Hello world\n" ;
+$name ='tanel';
+echo 'hello' . $name;
+echo "\n";
+$age = 20;
+echo ' Your name is ' . $name . ' and your are' . $age . ' years old ';
+echo "\n";
+echo "Your name is $name and you are $age years old\n";
+
+$var = 1;
+var_dump ($var);
+$var = 1.3;
+var_dump ($var);
+$var = true;
+var_dump ($var);
+$var = false;
+var_dump ($var);
+$bar = NULL;
+var_dump ($var);
+
+
+?>
