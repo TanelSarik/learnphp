af4b734bba8ddcf903166418021229232c1b5326
Tanel Sarik <tanel.sarik@tptlive.ee>
2025-09-12 16:15:01 +0300

    add dynamic posts

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+$title = 'world news';
+  $posts = [
+    [ 'title' => 'world news 1', 'author' => 'Pets', 'published'=>'11s.09.2025', 'body ' => 'some world news 1 body'],
+    [ 'title' => 'world news 2', 'author' => 'anns', 'published'=>'12.09.2025', 'body ' => 'some world news 2 body'],
+    [ 'title' => 'world news 3', 'author' => 'maalis', 'published'=>'10.09.2025', 'body ' => 'some world news 3 body'],
+    [ 'title' => 'world news 4', 'author' => 'mats', 'published'=>'13.09.2025', 'body ' => 'some world news 4 body'],
+  ];
+?>
+
+
 <?php include 'partials/header.php'; ?>
     <main class="container">
       <?php include 'partials/hero.php'; ?>
@@ -12,4 +23,3 @@
       </div>
     </main>
   <?php include 'partials/footer.php'; ?>
-  
--- us.php
+++ us.php
@@ -1,7 +1,17 @@
+<?php
+$title = 'us news';
+  $posts = [
+    [ 'title' => 'us news 1', 'author' => 'Pets', 'published'=>'11s.09.2025', 'body ' => 'some world news 1 body'],
+    [ 'title' => 'us news 2', 'author' => 'anns', 'published'=>'12.09.2025', 'body ' => 'some world news 2 body'],
+    [ 'title' => 'us news 3', 'author' => 'maalis', 'published'=>'10.09.2025', 'body ' => 'some world news 3 body'],
+    [ 'title' => 'us news 4', 'author' => 'mats', 'published'=>'13.09.2025', 'body ' => 'some world news 4 body'],
+  ];
+?>
+
+
 <?php include 'partials/header.php'; ?>
     <main class="container">
-      <?php include 'partials/hero.php'; ?>
-      <?php include 'partials/featured.php'; ?>
+     
       <div class="row g-5">
         <div class="col-md-8">
           <?php include 'partials/posts.php'; ?>
