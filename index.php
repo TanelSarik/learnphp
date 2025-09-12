51518f95c6e6d666f428cac47a6b9d7eec5c267d
Tanel Sarik <tanel.sarik@tptlive.ee>
2025-09-12 15:11:10 +0300

    simple html page with php

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
 
+$name = 'tanel';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?=$name?>!!!!</h1>
+    <ul>
+        <?php for($i=0;$i<10;$i++): ?>
+           <li><?=$i?></li>
+         <?php endfor ?>
+        <li></li>
+    </ul>
+</body>
+</html>
+
