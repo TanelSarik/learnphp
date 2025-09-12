066fde0e89bfd353a6a2eed77d1f332aa39cc473
Tanel Sarik <tanel.sarik@tptlive.ee>
2025-09-12 13:53:40 +0300

    Add magivc methods example

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ public static function cool(){
         $this->width = $width;
         $this->height = $height;
     }
+
      public function test(){
         var_dump($this->width);
       }
@@ -34,6 +35,35 @@ public static function cool(){
     public function volume (){
         return $this->height * $this->width * $this->lenght;
     }
+    public function __toString(){
+        return 'im a box';
+    }
+
+    public function __destruct(){
+        var_dump('object was destroyed');
+    }
+
+    public function __get($name){
+        var_dump("trying to get $name");
+        return 'some cool value';
+    }
+
+     public function __set($name,$value){
+        var_dump("trying to get $name with value $value");
+        
+    }
+
+    public function __call($name, $args){
+        var_dump("trying to call function $name with values", $args);
+    }
+    public static function __callstatic($name, $args){
+        var_dump("trying to call function $name with values", $args);
+    }
+
+    public function __invoke(...$args){
+      var_dump('i was called as function with args', $args);
+    }
+
 }
 
 class MetalBox extends box {
@@ -54,15 +84,18 @@ class IronBox extends MetalBox {
     public $weightPerUnit = 3;
 }
 
- box::$count = 1;
-var_dump($box1::$count);
+function makeBox(){
+    
+    $box1 = new Box();
+    var_dump($box1->potato);
+    $box-> hallo = 123;
+    $box->hello(1, 2, 3);
+    $box1(1, 2, 3, 4, 5);
+    echo $box1;
 
-box::$count = 2;
-var_dump(box::$count);
+}
 
-var_dump(box::$count);
-var_dump(Metalbox::$count);
-var_dump(Box::class);
-Box::cool();
-
-MetalBox::cool();
+makeBox();
+//unset($box1);
+$box1 = 1;
+var_dump('end of problem');
