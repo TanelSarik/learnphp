8055953e0156d32d142978a2433269ac4a9785cd
Tanel Sarik <tanel.sarik@tptlive.ee>
2025-09-15 16:06:57 +0300

    Add autoloading and names example

--- public/index.php
+++ public/index.php
@@ -12,28 +12,46 @@ function dump(...$args){
 }
 
 
+spl_autoload_register(function ($class) {
+       $class = substr($class, 4);
+       require_once __DIR__ . "/../src/$class.php";
+});
 
-switch($_SERVER['REQUEST_URI']) {
-    case '/':
-        $title = 'world news';
-  $posts = [
-    [ 'title' => 'world news 1', 'author' => 'Pets', 'published'=>'11s.09.2025', 'body ' => 'some world news 1 body'],
-    [ 'title' => 'world news 2', 'author' => 'anns', 'published'=>'12.09.2025', 'body ' => 'some world news 2 body'],
-    [ 'title' => 'world news 3', 'author' => 'maalis', 'published'=>'10.09.2025', 'body ' => 'some world news 3 body'],
-    [ 'title' => 'world news 4', 'author' => 'mats', 'published'=>'13.09.2025', 'body ' => 'some world news 4 body'],
-  ];
-        include __DIR__ . '/../views/index.php';
-        break;
-        case '/us':
-            $title = 'us news';
-  $posts = [
-    [ 'title' => 'us news 1', 'author' => 'Pets', 'published'=>'11s.09.2025', 'body ' => 'some world news 1 body'],
-    [ 'title' => 'us news 2', 'author' => 'anns', 'published'=>'12.09.2025', 'body ' => 'some world news 2 body'],
-    [ 'title' => 'us news 3', 'author' => 'maalis', 'published'=>'10.09.2025', 'body ' => 'some world news 3 body'],
-    [ 'title' => 'us news 4', 'author' => 'mats', 'published'=>'13.09.2025', 'body ' => 'some world news 4 body'],
-  ];
 
-            include __DIR__ . '/../views/index.php';
-           break;
-           default:echo '404 page ont found'; 
-}
+// require_once __DIR__ . '/../src/Router.php';
+// require_once __DIR__ . '/../src/DB.php';
+
+use App\Controllers\PublicController as PC;
+
+$router = new App\Router();
+dump($router);
+$db = new App\DB();
+dump($db);
+$controller = New PC();
+dump($controller);
+
+
+// switch($_SERVER['REQUEST_URI']) {
+//     case '/':
+//         $title = 'world news';
+//   $posts = [
+//     [ 'title' => 'world news 1', 'author' => 'Pets', 'published'=>'11s.09.2025', 'body ' => 'some world news 1 body'],
+//     [ 'title' => 'world news 2', 'author' => 'anns', 'published'=>'12.09.2025', 'body ' => 'some world news 2 body'],
+//     [ 'title' => 'world news 3', 'author' => 'maalis', 'published'=>'10.09.2025', 'body ' => 'some world news 3 body'],
+//     [ 'title' => 'world news 4', 'author' => 'mats', 'published'=>'13.09.2025', 'body ' => 'some world news 4 body'],
+//   ];
+//         include __DIR__ . '/../views/index.php';
+//         break;
+//         case '/us':
+//             $title = 'us news';
+//   $posts = [
+//     [ 'title' => 'us news 1', 'author' => 'Pets', 'published'=>'11s.09.2025', 'body ' => 'some world news 1 body'],
+//     [ 'title' => 'us news 2', 'author' => 'anns', 'published'=>'12.09.2025', 'body ' => 'some world news 2 body'],
+//     [ 'title' => 'us news 3', 'author' => 'maalis', 'published'=>'10.09.2025', 'body ' => 'some world news 3 body'],
+//     [ 'title' => 'us news 4', 'author' => 'mats', 'published'=>'13.09.2025', 'body ' => 'some world news 4 body'],
+//   ];
+
+//             include __DIR__ . '/../views/index.php';
+//            break;
+//            default:echo '404 page ont found'; 
+// }
